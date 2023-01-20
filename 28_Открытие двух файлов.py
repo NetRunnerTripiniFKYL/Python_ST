76f5c807af9169fd73a2d031499dd2522302bedf
NetRunnerTripiniFKYL <120100539+NetRunnerTripiniFKYL@users.noreply.github.com>
2023-01-20 22:25:37 +0700

    ---

--- 28_Открытие двух файлов.py
+++ 28_Открытие двух файлов.py
@@ -4,5 +4,6 @@
 
 with open(r"A:\Users\lakus\Documents\Python\ST\r1.txt", "r") as a, open(r"A:\Users\lakus\Documents\Python\ST\List.txt", "w") as b:
     for line in a:
-        if line.startswith("service"):
+        if line.startswith("service"): #Тут мы записали в другой файл только первые 4 строки, так как указали что переписывать только если строка начинается с service
             b.write(line)
+
